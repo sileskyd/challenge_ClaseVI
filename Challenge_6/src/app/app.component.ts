388adfce24bab425f0fd0bc0f63810f9715e825c
DanielS <sileskyd@gmail.com>
2024-05-13 15:07:27 -0600

    Se crean componentes para el diseño de la página

--- Challenge_6/src/app/app.component.ts
+++ Challenge_6/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HomeComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'Challenge_6';
