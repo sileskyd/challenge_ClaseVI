08b67a7dbbce3113695f674c3140c64f30486391
DanielS <sileskyd@gmail.com>
2024-05-13 18:23:23 -0600

    Creacion de nuevos componentes

--- Challenge_6/src/app/app.component.ts
+++ Challenge_6/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from "./footer/footer.component";
+import { CardComponent } from './card/card.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomeComponent, NavbarComponent]
+    imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent, CardComponent]
 })
 export class AppComponent {
   title = 'Challenge_6';
